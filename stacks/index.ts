f84fcd3c5ddf083a352a05f2b53ed9e0fbdf1384
Evan Young <715756+YourBassHertz@users.noreply.github.com>
2022-06-30 10:43:10 -0700

    Adds github actions and adds a tag.

--- stacks/index.ts
+++ stacks/index.ts
@@ -1,7 +1,9 @@
 import { MyStack } from "./MyStack";
 import { App } from "@serverless-stack/resources";
+import * as cdk from "@aws-cdk";
 
 export default function (app: App) {
+  cdk.Tags.of(app).add("my-tag", `${app.stage}-${app.region}`);
   app.setDefaultFunctionProps({
     runtime: "nodejs16.x",
     srcPath: "backend",
