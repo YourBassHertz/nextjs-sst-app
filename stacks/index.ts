04492f08852233fb30e19ab74b41cdf25496c268
Evan Young <715756+YourBassHertz@users.noreply.github.com>
2022-06-30 10:51:54 -0700

    Updates tagging method.

--- stacks/index.ts
+++ stacks/index.ts
@@ -1,6 +1,6 @@
 import { MyStack } from "./MyStack";
 import { App } from "@serverless-stack/resources";
-import * as cdk from "@aws-cdk";
+import * as cdk from "aws-cdk-lib";
 
 export default function (app: App) {
   cdk.Tags.of(app).add("my-tag", `${app.stage}-${app.region}`);
